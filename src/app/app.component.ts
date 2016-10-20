afccff1bfdb65aba793ebe4d096f3bcddf5afe37
Czarniecki, Jacek <jacek.czarniecki@ihsmarkit.com>
2016-10-20 02:27:30 +0200

    refatoring: Extract AppComponent template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'cm-app',
-    template: `
-    <div>
-        <nav class='navbar navbar-default'>
-            <div class='container-fluid'>
-                <a class='navbar-brand' [routerLink]="['/']">{{appTitle}}</a>
-                <ul class='nav navbar-nav'>
-                    <li><a [routerLink]="['/projects']">Projects List</a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class='container'>
-            <router-outlet></router-outlet>
-        </div>
-     </div>
-     `
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
     appTitle: string = 'Coverage Monitoring';
